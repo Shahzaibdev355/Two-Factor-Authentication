e684d5dabc821ca0ca1c46bc522b69b3b59e1820
Shahzaib <shahzaibsheikh355@gmail.com>
2026-02-27 16:19:48 +0500

    api updated

--- src/repositories/cron.repository.ts
+++ src/repositories/cron.repository.ts
@@ -4,7 +4,7 @@ import config from '../config/index';
 export class CronRepository {
     async pingHF(): Promise<boolean> {
         try {
-            await axios.get(config.HF_BACKEND_URL + "/health");
+            await axios.get(config.HF_BACKEND_URL);
             return true;
         } catch (error) {
             console.error("HF Ping Failed:", error);
